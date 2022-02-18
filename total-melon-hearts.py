d9339f07cbee8b6a62e895f9c09e6c3f8f71c9b3
Mauricio Villa <vbamauricio@gmail.com>
2022-02-18 13:07:54 -0300

    Renamed variables for better styling

--- total-melon-hearts.py
+++ total-melon-hearts.py
@@ -4,79 +4,86 @@ from bs4 import BeautifulSoup
 from termcolor import colored
 
 # Artist ID from the ?artistId=
-artistId = input('Type in the artist id: ')
+artist_id = input('Type in the artist id: ')
+
 
 # Global
-def most_frequent(list): 
-	counter = 0
-	num = list[0] 
-	for i in list: 
-		curr_frequency = list.count(i) 
-		if(curr_frequency > counter): 
-			counter = curr_frequency 
-			num = i 
-	return num 
+def most_frequent(list):
+    counter = 0
+    num = list[0]
+    for i in list:
+        curr_frequency = list.count(i)
+        if (curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+    return num
+
 
 def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-artistList = []
-songList = []
-heartsList = []
+
+artist_list = []
+song_list = []
 
 n = 1
 
 # MelOn Headers
-melonheaders = {
-    'Referer': 'https://www.melon.com/artist/timeline.htm?artistId=' + artistId,
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+melon_headers = {
+    'Referer':
+    'https://www.melon.com/artist/timeline.htm?artistId=' + artist_id,
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 }
 
 # Paging URL API
-pagsongUrl = 'https://www.melon.com/artist/songPaging.htm'
-pagingQuerys = {
-    'startIndex' : '1',
-    'pageSize' : '2000',
-    'orderBy' : 'ISSUE_DATE',
-    'artistId' : artistId
+paging_url = 'https://www.melon.com/artist/songPaging.htm'
+paging_query = {
+    'startIndex': '1',
+    'pageSize': '2000',
+    'orderBy': 'ISSUE_DATE',
+    'artistId': artist_id
 }
 
 # Hearts URL API
-songUrl = 'https://www.melon.com/commonlike/getSongLike.json'
+song_url = 'https://www.melon.com/commonlike/getSongLike.json'
 
 # Stars URL API
-starsUrl = 'https://www.melon.com/artist/getArtistFanNTemper.json?artistId=' + artistId
+stars_url = 'https://www.melon.com/artist/getArtistFanNTemper.json?artistId=' + artist_id
 
-pagingSongs = requests.get(pagsongUrl, params = pagingQuerys, headers = melonheaders).text
-pagingResp = BeautifulSoup(pagingSongs, 'html.parser')
-for tr in pagingResp.select('div.tb_list table tbody tr'):
+paging_songs = requests.get(paging_url,
+                            params=paging_query,
+                            headers=melon_headers).text
+paging_response = BeautifulSoup(paging_songs, 'html.parser')
+for tr in paging_response.select('div.tb_list table tbody tr'):
     for a in tr.select('td.t_left div.wrap.wrapArtistName #artistName a'):
-        artistFeat = a.text
-        artistList.append(artistFeat)
-    songId = tr.select('button.btn_icon.like')[0].attrs['data-song-no']
-    songList.append(songId)
-artistName = colored(most_frequent(artistList), attrs=['bold'])
-print('Collecting the songs by', artistName)
-print(len(songList), 'songs found on their profile')
+        artist_feat = a.text
+        artist_list.append(artist_feat)
+    song_id = tr.select('button.btn_icon.like')[0].attrs['data-song-no']
+    song_list.append(song_id)
+artist_name = colored(most_frequent(artist_list), attrs=['bold'])
+print(f'Collecting the songs by {artist_name}')
+print(f'{len(song_list)} songs found on their profile')
 
-artistStars = requests.get(starsUrl, headers = melonheaders).text
-jsonStars = json.loads(artistStars)
-starsTotal = jsonStars['fanInfo']['SUMMCNT']
+artist_stars_api = requests.get(stars_url, headers=melon_headers).text
+artist_stars_json = json.loads(artist_stars_api)
+total_stars = artist_stars_json['fanInfo']['SUMMCNT']
 
-for songIds in chunks(songList, 100):
-    x = len(songIds)
+total_hearts = 0
+for song_ids in chunks(song_list, 100):
+    x = len(song_ids)
     print(f'Analizing data from song {n}-{n+x-1}')
     n += x
-    songIdparams = {
-        'contsIds' : songIds
-    }
-    songHearts = requests.get(songUrl, params = songIdparams, headers = melonheaders).text
-    jsonHearts = json.loads(songHearts)
-    for h in jsonHearts['contsLike']:
+    song_id_params = {'contsIds': song_ids}
+    hearts_song = requests.get(song_url,
+                               params=song_id_params,
+                               headers=melon_headers).text
+    hearts_song_json = json.loads(hearts_song)
+    for h in hearts_song_json['contsLike']:
         hearts = h['SUMMCNT']
-        if hearts > 0:
-            heartsList.append(hearts)
-heartsTotal = sum(heartsList)
+        total_hearts += hearts
 
-print(artistName, 'has', f'{heartsTotal:,}', colored('hearts', 'red'), 'and', f'{starsTotal:,}', colored('stars', 'yellow'), 'on', colored('MelOn', 'green'))
+print(
+    f'{artist_name} has {total_hearts:,} {colored("hearts", "red")} and {total_stars:,} {colored("stars", "yellow")} on {colored("MelOn", "green")}'
+)
